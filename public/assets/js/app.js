93c20c7f7305414bd01650295a02262fc0b47bd2
MrMendonez <dmendonez@gmail.com>
2016-03-25 03:20:08 -0400

    got add input field to work

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,9 +2,16 @@ angular.module("todoApp", [])
   .controller("TodoListController", function($scope) {
     var todoList = this;
     todoList.todoItems = [
-    "My Homework",
-    "My Laundry",
-    "Pick up the thing",
-    "Buy the other thing"
+      { name: "My Homework", done: true },
+      { name: "My Laundry", done: false },
+      { name: "One Thing", done: false },
+      { name: "Another Thing", done: false }
     ]
+
+    todoList.addTodo = function() {
+      todoList.todoItems.push(
+        { name: todoList.todoText, done: false }
+      )
+      console.log('This worked');
+    };
   });
